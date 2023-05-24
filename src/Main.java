cde4b126a95faaf206dd8479f8ed09fb080f421c
Михаил Синёв <msinev@masterdm.ru>
2023-05-24 15:58:55 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BmiService service = new BmiService();
         double myBmi = service.bmi(95, 1.85);
-        System.out.println("ИМТ = " + myBmi );
-        }
+        System.out.println("ИМТ = " + myBmi);
+    }
 
 }
